85081aff210506857aede25574d224d0f71a2bbf
MihdarAbbasher <mihdar.ph@gmail.com>
2024-01-25 03:49:44 -0800

    t2update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -24,7 +24,8 @@ def do_deploy(archive_path):
         run('sudo mkdir -p {}/'.format(f_path))
         # uncompress archive
         run('sudo tar -xzf /tmp/{}.tgz -C {}/'.format(f_name, f_path))
-        # run('sudo mv {}/web_static/* {}/'.format(f_path, f_path)
+        run('sudo mv {}/web_static/* {}/'.format(f_path, f_path)
+        run('sudo rm -rf {}/web_static/'.format(f_path))
         run('sudo rm /tmp/{}.tgz'.format(f_name))
         # remove existing sym link & create new one
         run('sudo rm -rf /data/web_static/current')
